4dae4480c2631f225aae8c2d06f2e58b8eed01a6
danielViolist <daniel.violist3@gmail.com>
2022-11-25 19:19:52 -0500

    AddFloor and New funcs #42

--- db/floors.go
+++ db/floors.go
@@ -1,5 +1,11 @@
 package db
 
+import (
+	"context"
+	"log"
+	"time"
+)
+
 type floor struct {
 	name string
 	/*
@@ -11,12 +17,54 @@ type floor struct {
 }
 
 /*
+Creates a new floor.
+Will check for valid name and file.
+Returns error if things went wrong.
+*/
+func (db *base) New(name, deviceList string) (flo floor, err error) {
+	flo = floor{
+		name:           "",
+		deviceListFile: "",
+	}
+	// Check floor name
+	if err = db.checkFloor(name); err != nil {
+		return
+	}
+	// Check file name
+	if err = checkFile(deviceList); err != nil {
+		return
+	}
+
+	// Everything is good, so return the floor data
+	flo.name = name
+	flo.deviceListFile = deviceList
+	return
+}
+
+/*
 Adds a Floor to the database.
 Returns an error if things
 went wrong. nil otherwise.
 */
-func (db *dbase) AddFloor(name, deviceFile string) error {
-	// TODO
+func (db *dbase) AddFloor(fl floor) error {
+	if !db.opened {
+		db.Open()
+	}
+
+	query := "INSERT INTO floors(name, devicelist) VALUES (?, ?)"
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelfunc()
+	stmt, err := db.sqldb.PrepareContext(ctx, query)
+	if err != nil {
+		log.Printf("Error %s when preparing SQL statement", err)
+		return err
+	}
+	defer stmt.Close()
+
+	_, err = stmt.ExecContext(ctx, fl.name, fl.deviceListFile)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
