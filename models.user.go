cb95089b6b6d6d17571c7d02b0e8369d8671ba28
jespeer <jespeer@email.sc.edu>
2022-12-01 18:22:22 -0500

    notes

--- models.user.go
+++ models.user.go
@@ -7,24 +7,26 @@ import (
 	"strings"
 )
 
+/*
+user struct has a username and a password
+*/
 type user struct {
 	Username string `json:"username"`
 	Password string `json:"-"`
 }
 
-// For this demo, we're storing the user list in memory
-// We also have some users predefined.
-// In a real application, this list will most likely be fetched
-// from a database. Moreover, in production settings, you should
-// store passwords securely by salting and hashing them instead
-// of using them as we're doing in this demo
+/*
+List of users
+*/
 var userList = []user{
 	user{Username: "user1", Password: "pass1"},
 	user{Username: "user2", Password: "pass2"},
 	user{Username: "user3", Password: "pass3"},
 }
 
-// Check if the username and password combination is valid
+/*
+Checks if the username and password combination is valid
+*/
 func isUserValid(username, password string) bool {
 	for _, u := range userList {
 		if u.Username == username && u.Password == password {
@@ -34,8 +36,9 @@ func isUserValid(username, password string) bool {
 	return false
 }
 
-// Register a new user with the given username and password
-// NOTE: For this demo, we
+/*
+Registers a new user with given username/password by adding to list
+*/
 func registerNewUser(username, password string) (*user, error) {
 	if strings.TrimSpace(password) == "" {
 		return nil, errors.New("The password can't be empty")
@@ -44,13 +47,14 @@ func registerNewUser(username, password string) (*user, error) {
 	}
 
 	u := user{Username: username, Password: password}
-
 	userList = append(userList, u)
 
 	return &u, nil
 }
 
-// Check if the supplied username is available
+/*
+Check if the inputted username is available
+*/
 func isUsernameAvailable(username string) bool {
 	for _, u := range userList {
 		if u.Username == username {
