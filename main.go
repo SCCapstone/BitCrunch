d9e55c49d1e81511b8a6fb7c1e42fd8516d43bc6
jespeer <jespeer@email.sc.edu>
2023-01-29 18:57:59 -0500

    working on integrating floors with front-end

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"strconv"
 
 	middleware "github.com/SCCapstone/BitCrunch/middleware"
-	models "github.com/SCCapstone/BitCrunch/models"
+	// models "github.com/SCCapstone/BitCrunch/models"
 	db "github.com/SCCapstone/BitCrunch/db"
 	"github.com/gin-gonic/gin"
 )
@@ -87,11 +87,11 @@ func InitializeRoutes() {
 
 		// Handle POST requests at /u/add_layer, ensure user is logged in using middleware
 		// Add the layer
-		userRoutes.POST("/add_layer", middleware.EnsureLoggedIn(), AddLayer)
+		// userRoutes.POST("/add_layer", middleware.EnsureLoggedIn(), AddLayer)
 
 		// Handle POST requests at /u/view_layer, ensure user is logged in using middleware
 		// Render the image to map
-		userRoutes.POST("/view_layer", middleware.EnsureLoggedIn(), viewLayer)
+		// userRoutes.POST("/view_layer", middleware.EnsureLoggedIn(), viewLayer)
 
 		// Handle GET requests at /u/register, ensure user is not logged in using middleware
 		//Render the registration page
@@ -213,25 +213,26 @@ func GenerateSessionToken() string {
 Gets the proper floor from the list of floors based on its name
 Renders the proper floor image onto the map
 */
-func viewLayer(c *gin.Context) {
-	name := c.PostForm("l_name")
-	floors := models.GetAllFloors()
-	for i := 0; i < len(floors); i++ {
-		if floors[i].Name == name {
-			Render(c, gin.H{
-				"title":   "Map",
-				"payload": floors,
-				"Image":   "../" + floors[i].ImageFile,
-			}, "index.html")
-		}
-	}
-}
+// func viewLayer(c *gin.Context) {
+// 	name := c.PostForm("l_name")
+// 	floors := models.GetAllFloors()
+// 	for i := 0; i < len(floors); i++ {
+// 		if floors[i].Name == name {
+// 			Render(c, gin.H{
+// 				"title":   "Map",
+// 				"payload": floors,
+// 				"Image":   "../" + floors[i].ImageFile,
+// 			}, "index.html")
+// 		}
+// 	}
+// }
 
 /*
 Renders the index with updated layer values
 */
 func showMap(c *gin.Context) {
-	floors := models.GetAllFloors()
+	fmt.Print(db.GetAllFloors())
+	floors, _ := db.GetAllFloors()
 
 	Render(c, gin.H{
 		"title":   "Map",
@@ -244,19 +245,19 @@ Adds a layer with a layer name inputted from the user
 Saves uploaded image to static/assets folder
 Creates a new floor and adds it to the list of floors, calls showMap to render the map with updates
 */
-func AddLayer(c *gin.Context) {
-	layer_name := c.PostForm("layer_name")
+// func AddLayer(c *gin.Context) {
+// 	layer_name := c.PostForm("layer_name")
 
-	file, err := c.FormFile("layer_image")
-	if err != nil {
-		fmt.Println(err)
-	}
+// 	file, err := c.FormFile("layer_image")
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
 
-	err = c.SaveUploadedFile(file, "static/assets/"+file.Filename)
-	if err != nil {
-		fmt.Println(err)
-	}
+// 	err = c.SaveUploadedFile(file, "static/assets/"+file.Filename)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
 
-	models.CreateNewFloor(layer_name, "static/assets/"+file.Filename)
-	showMap(c)
-}
+// 	models.CreateNewFloor(layer_name, "static/assets/"+file.Filename)
+// 	showMap(c)
+// }
