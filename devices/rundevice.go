3705d99ab4ec633953056a5f25a455fae98da83e
Trey Sturman <tssturman@gmail.com>
2023-02-11 16:05:52 -0500

    #94

--- devices/rundevice.go
+++ devices/rundevice.go
@@ -2,7 +2,6 @@ package devices
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"os/exec"
 	"strings"
@@ -11,10 +10,10 @@ import (
 // function takes in the exact name of the script, searches for it in assets
 // After found, runs each line through the OS's commandline
 // Importantly, any line beginning with # are discarded; they are considered comments
-func RunFromScript(filename string) (error, string) {
-	// gotta go to the right file!
+func RunFromScript(filename string, targetIP string) (error, string) {
+	IPaddress := "<IPADDRESS>" // for better usage across many files and devices
 	outputAll := ""
-	betterFilename := "/static/assets/" + filename
+	betterFilename := "/static/assets/" + filename // gotta get the right file!
 	// the paaram should be the file name, no txt
 	file, err := os.Open(betterFilename)
 	if err != nil {
@@ -26,10 +25,17 @@ func RunFromScript(filename string) (error, string) {
 	for fileScanner.Scan() {           // this moves until end of file
 		//filescanner.Text() = current line
 		line := strings.Fields(fileScanner.Text()) // split by space, tab, newline, etc.
+		IPfound, index := contains(line, IPaddress)
+		if IPfound && index != -1 {
+			line[index] = IPaddress // place the parameter IPaddress properly into the script
+		}
+		// make sure to test this!
 		if string(line[0]) == ("#") || line == nil {
 			continue // This line is a comment or empty! don't do anything!
 		}
 		var cmd *exec.Cmd
+		//https://stackoverflow.com/questions/22781788/how-could-i-pass-a-dynamic-set-of-arguments-to-gos-command-exec-command
+		//- for arguments in exec.Command
 		if len(line) == 1 {
 			// just solo exec.Command(line)
 			cmd = exec.Command(line[0])
@@ -40,19 +46,22 @@ func RunFromScript(filename string) (error, string) {
 		if cmd == nil {
 			continue
 		}
-		output, err := cmd.CombinedOutput() // this works, even w empty cmd!
-		//https://stackoverflow.com/questions/22781788/how-could-i-pass-a-dynamic-set-of-arguments-to-gos-command-exec-command
-		// :) - for arguments in exec.Command
-
+		output, err := cmd.CombinedOutput() // this works, bc command is NEVER empty! (kinda)
+		// just in case, there's a check right above
 		if err != nil {
 			file.Close()
 			return err, string(output)
 		}
-	}
-
-	if err != nil {
-		file.Close()
-		fmt.Print("do something")
+		outputAll += string(output) // move the output into the full output
 	}
 	return nil, outputAll
+}
+
+func contains(fullList []string, soloString string) (bool, int) {
+	for index, a := range fullList {
+		if a == soloString {
+			return true, index
+		}
+	}
+	return false, -1
 }
