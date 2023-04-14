0fa4873e5e644159ffceb7c2f9846ccb82a33ae4
danielViolist <daniel.violist3@gmail.com>
2023-04-13 22:21:51 -0400

    changed to go IPs to support IPv6 #130

--- db/devices.go
+++ db/devices.go
@@ -2,11 +2,9 @@ package db
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
-	"io/ioutil"
+	"net"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -14,7 +12,7 @@ const devices = "devices.db"
 
 type device struct {
 	name string
-	ip   string // static IP of the device
+	ip   net.IP // static ip of device
 	// This is the NAME of the image file
 	// NOT the actual image in memory
 	image string
@@ -24,23 +22,16 @@ type device struct {
 }
 
 /*
-Getter used for comparisons between devices in Unit Testing
-*/
-func GetDeviceName(d device) string {
-	return d.name
-}
-
-/*
 Add a device to the database.
 Returns error if things went wrong.
 nil otherwise
 */
 func CreateDevice(name, ip, image, floorNm string) (dev device, err error) {
 	// Check device name
-	if err = CheckDevice(name, floorNm); err != nil {
-		return
-	}
-	// Check IP formatting and IP not already in database
+	// if err = CheckDevice(name); err != nil {
+	// 	return
+	// }
+	// Check IP formatting
 	if err = CheckIP(ip); err != nil {
 		return
 	}
@@ -52,7 +43,7 @@ func CreateDevice(name, ip, image, floorNm string) (dev device, err error) {
 	// All checks are good, creating the
 	// floor and writing to db
 	dev.name = name
-	dev.ip = ip
+	dev.ip = net.ParseIP(ip)
 	dev.image = image
 	dev.floorName = floorNm
 
@@ -80,7 +71,7 @@ func writeDevice(d device) (err error) {
 
 	// Creating the string from the device details
 	// will append to the file
-	writeString := fmt.Sprintf("%s\t%s\t%s\t%s\n", d.name, d.ip, d.image, d.floorName)
+	writeString := fmt.Sprintf("%s\t%s\t%s\t%s\n", d.name, d.ip.String(), d.image, d.floorName)
 	_, err = fil.WriteString(writeString)
 	if err != nil {
 		return err
@@ -95,130 +86,38 @@ a valid IP format. Returns
 nil if it's good. Error otherwise.
 */
 func CheckIP(ip string) error {
-	// checking if IP already matches another device
-	ips, err := GetAllIPs()
-	for _, i := range ips {
-		if ip == i {
-			return fmt.Errorf("IP is already in use for a device.")
-		}
+	// This function converts to a go IP type.
+	// will be nil if parsing failed.
+	check := net.ParseIP(ip)
+	if check == nil {
+		return fmt.Errorf("Failed to parse IP address.")
 	}
-	// checking IP format valid
-	checkIP := strings.Split(ip, ".")
-	if len(checkIP) != 4 {
-		return fmt.Errorf("IP format is invalid.")
-	}
-	// Getting each octet to verify that it is in
-	// a valid IP range of 0-255
-	first, err := strconv.Atoi(checkIP[0])
-	if err != nil || first < 0 || first > 255 {
-		return fmt.Errorf("IP format is invalid.")
-	}
-	second, err := strconv.Atoi(checkIP[1])
-	if err != nil || second < 0 || second > 255 {
-		return fmt.Errorf("IP format is invalid.")
-	}
-	third, err := strconv.Atoi(checkIP[2])
-	if err != nil || third < 0 || third > 255 {
-		return fmt.Errorf("IP format is invalid.")
-	}
-	fourth, err := strconv.Atoi(checkIP[3])
-	if err != nil || fourth < 0 || fourth > 255 {
-		return fmt.Errorf("IP format is invalid.")
-	}
-
-	// All should be good
+	// IP is not nil so everything is good
 	return nil
 }
 
 /*
 This checks to ensure that no other
-device has the same name in the db for a specific floor.
+device has the same name in the db.
 Return nil if good, error otherwise.
-*/
-
-func CheckDevice(name, floorNm string) error {
-	fi, err := os.Open("devices/" + floorNm + ".txt")
-	if err != nil {
-		return err
-	}
-	defer fi.Close()
-	scan := bufio.NewScanner(fi)
-	var line []string
-	// Finding each device with the given floor name
-	firstLine := true
-	for scan.Scan() {
-		if firstLine == true {
-			firstLine = false
-			continue
-		}
-		line = strings.Split(scan.Text(), "\t")
-		// Device found, append it to the slice
-		if len(line) > 1 {
-			d := device{
-				name:      line[0],
-				ip:        line[1],
-				image:     line[2],
-				floorName: line[3],
-			}
-			if name == d.name {
-				return errors.New("Device name is not unique to floor.")
-			}
-		}
-	}
-
-	return nil
-}
-
-func EditDevice(name, newName, newIP, newImage, floorNm string) {
-	fi, err := ioutil.ReadFile("devices/" + floorNm + ".txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	lines := strings.Split(string(fi), "\n")
-	firstLine := true
-
-	for i, line := range lines {
-		if firstLine == true {
-			firstLine = false
-			continue
-		}
-		splitLine := strings.Split(line, "\t")
-		fmt.Println(splitLine, len(splitLine))
-		if len(splitLine) > 1 {
-			d := device{
-				name:      splitLine[0],
-				ip:        splitLine[1],
-				image:     splitLine[2],
-				floorName: splitLine[3],
-			}
-			if d.name == name {
-				writeString := fmt.Sprintf("%s\t%s\t%s\t%s", newName, newIP, newImage, d.floorName)
-				lines[i] = writeString
-			}
-		}
-	}
-	output := strings.Join(lines, "\n")
-	err = ioutil.WriteFile("devices/"+floorNm+".txt", []byte(output), 0644)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
+// */
+// func CheckDevice(name string) error {
+// return nil
+// }
 
 /*
 Remove a device from the database.
 Returns nil if it was sucessful.
 Error otherwise.
 */
-func DeleteDevice(name, floorNm string) error {
+func DeleteDevice(name string) error {
 	// making temp file
 	delMe, err := os.Create(fmt.Sprintf("temp%s.tmp", name))
-	newName := "devices/" + floorNm + ".txt"
 	if err != nil {
 		return err
 	}
 	// Opening db file
-	fi, err := os.Open("devices/" + floorNm + ".txt")
+	fi, err := os.Open(devices)
 	if err != nil {
 		return err
 	}
@@ -228,61 +127,42 @@ func DeleteDevice(name, floorNm string) error {
 	for scan.Scan() {
 		line = scan.Text()
 		if strings.Split(line, "\t")[0] != name {
-			delMe.WriteString(line + "\n")
+			delMe.WriteString(line)
 		}
 	}
 
 	// Finished coping the device data to new file
 	// Cleaning up
 	fi.Close()
-	err = os.Remove("devices/" + floorNm + ".txt")
+	err = os.Remove(devices)
 	if err != nil {
 		return err
 	}
 
 	// Renaming the new file withouth the
 	// deleted device to the devices.db name
-	delMe.Close()
-	err = os.Rename(delMe.Name(), newName)
+	err = os.Rename(delMe.Name(), devices)
 	if err != nil {
 		return err
 	}
 
 	// All good, clean up
+	delMe.Close()
 	return nil
 }
 
-func GetIP(name string) string {
-	floors, err := GetAllFloors()
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, floor := range floors {
-		devices, _ := GetAllDevicesForFloor(floor.name)
-		for _, device := range devices {
-			if device.name == name {
-				return device.ip
-			}
-		}
-	}
-	return ""
-}
-
-func GetImage(name string) string {
-	floors, err := GetAllFloors()
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, floor := range floors {
-		devices, _ := GetAllDevicesForFloor(floor.name)
-		for _, device := range devices {
-			if device.name == name {
-				return device.image
-			}
-		}
-	}
-	return ""
-}
+/*
+Returns the IP of a device
+given a name.
+Pretty useless function, but here it is.
+*/
+// func GetIP(name string) (string, error) {
+// 	dev, err := ReadDevice(name)
+// 	if err != nil {
+// 		return "", fmt.Errorf("Device not found!")
+// 	}
+// 	return dev.ip, nil
+// }
 
 /*
 This function can be used to get every
@@ -310,31 +190,14 @@ func GetAllDevicesForFloor(floorNm string) (devs []device, err error) {
 		}
 		line = strings.Split(scan.Text(), "\t")
 		// Device found, append it to the slice
-		if len(line) > 1 {
-			d := device{
-				name:      line[0],
-				ip:        line[1],
-				image:     line[2],
-				floorName: line[3],
-			}
-			devs = append(devs, d)
+		d := device{
+			name:      line[0],
+			ip:        net.ParseIP(line[1]),
+			image:     line[2],
+			floorName: line[3],
 		}
+		devs = append(devs, d)
 	}
 
 	return devs, nil
-}
-
-func GetAllIPs() (myDevices []string, err error) {
-	var deviceIPs = []string{}
-	floors, err := GetAllFloors()
-	if err != nil {
-		return deviceIPs, err
-	}
-	for _, floor := range floors {
-		devices, _ := GetAllDevicesForFloor(floor.name)
-		for _, device := range devices {
-			deviceIPs = append(deviceIPs, device.ip)
-		}
-	}
-	return deviceIPs, nil
 }
