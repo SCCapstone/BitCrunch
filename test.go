b6a71693e37fc7315d627d125ed2dfe0b426d695
Ethan Speer <70123091+jespeer@users.noreply.github.com>
2023-04-20 15:02:31 -0400

    Delete test.go

--- test.go
+++
@@ -1,7 +0,0 @@
-package main
-// import "fmt"
-// import db "github.com/SCCapstone/BitCrunch/db"
-// func main() {
-//     fmt.Println("hello world")
-// 	db.EditDeviceCoordinates("d1", "floor1", "500", "200")
-// }
